a1b808a176338d8c6eca5f52f206c4bb5ecd8fd9
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2023-06-08 16:01:52 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# local_turism
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/local_turism/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.local_turism
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
