c10013e5ace0ff2d40837a20a8a6d5f982ffefc1
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2023-06-08 20:00:19 -0300

    organized main class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:local_turism/views/home_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Turismo Alagoas',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyHomePageWidget(),
+      },
     );
   }
 }
