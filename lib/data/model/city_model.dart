805a4902237e2dcc18d306fafb32d7bae00885e4
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2023-06-10 18:09:59 -0300

    Model update

--- lib/data/model/city_model.dart
+++ lib/data/model/city_model.dart
@@ -5,54 +5,60 @@
 import 'dart:convert';
 
 class CityModel {
-    final List<City> cities;
+  final List<City> cities;
 
-    CityModel({
-        required this.cities,
-    });
+  CityModel({
+    required this.cities,
+  });
 
-    factory CityModel.fromRawJson(String str) => CityModel.fromJson(json.decode(str));
+  factory CityModel.fromRawJson(String str) =>
+      CityModel.fromJson(json.decode(str));
 
-    String toRawJson() => json.encode(toJson());
+  String toRawJson() => json.encode(toJson());
 
-    factory CityModel.fromJson(Map<String, dynamic> json) => CityModel(
+  factory CityModel.fromJson(Map<String, dynamic> json) => CityModel(
         cities: List<City>.from(json["cities"].map((x) => City.fromJson(x))),
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "cities": List<dynamic>.from(cities.map((x) => x.toJson())),
-    };
+      };
 }
 
 class City {
-    final String name;
-    final String mainImage;
-    final List<String> cityFacts;
-    final List<String> extraImages;
+  final int id;
+  final String name;
+  final String mainImage;
+  final String cityDescription;
+  final List<String> cityFacts;
+  final List<String> extraImages;
 
-    City({
-        required this.name,
-        required this.mainImage,
-        required this.cityFacts,
-        required this.extraImages,
-    });
+  City({
+    required this.id,
+    required this.name,
+    required this.mainImage,
+    required this.cityDescription,
+    required this.cityFacts,
+    required this.extraImages,
+  });
 
-    factory City.fromRawJson(String str) => City.fromJson(json.decode(str));
+  factory City.fromRawJson(String str) => City.fromJson(json.decode(str));
 
-    String toRawJson() => json.encode(toJson());
+  String toRawJson() => json.encode(toJson());
 
-    factory City.fromJson(Map<String, dynamic> json) => City(
+  factory City.fromJson(Map<String, dynamic> json) => City(
+        id: json["id"],
         name: json["name"],
         mainImage: json["main_image"],
+        cityDescription: json["city_description"],
         cityFacts: List<String>.from(json["city_facts"].map((x) => x)),
         extraImages: List<String>.from(json["extra_images"].map((x) => x)),
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "name": name,
         "main_image": mainImage,
         "city_facts": List<dynamic>.from(cityFacts.map((x) => x)),
         "extra_images": List<dynamic>.from(extraImages.map((x) => x)),
-    };
+      };
 }
-
