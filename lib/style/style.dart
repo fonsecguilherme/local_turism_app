c37db08dbaf6e76f21ada08a0d180db839b128e8
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2023-06-10 18:12:10 -0300

    added banner text styles

--- lib/style/style.dart
+++ lib/style/style.dart
@@ -15,4 +15,16 @@ class Style {
     fontSize: 20,
     fontWeight: FontWeight.w700,
   );
+
+  static const bannerCityNameStyle = TextStyle(
+    color: AppColors.white,
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const bannerCityDescriptionStyle = TextStyle(
+    color: AppColors.white,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
 }
