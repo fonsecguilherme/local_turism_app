d88887ec222352395aaf93199727eaeebb8d00ff
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2023-06-11 12:22:30 -0300

    WIP data page

---
+++ lib/views/pages/data_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:local_turism/data/model/city_model.dart';
+import 'package:local_turism/views/widgets/city_card.dart';
+
+class DataPage extends StatefulWidget {
+  final AsyncSnapshot<CityModel?> snapshot;
+
+  const DataPage({super.key, required this.snapshot});
+
+  @override
+  State<DataPage> createState() => _DataPageState();
+}
+
+class _DataPageState extends State<DataPage> {
+  @override
+  Widget build(BuildContext context) {
+    List<City> cities = widget.snapshot.data!.cities;
+
+    return ListView.builder(
+      itemCount: cities.length,
+      itemBuilder: (context, index) => CityCard(
+        city: cities[index],
+      ),
+    );
+  }
+}
