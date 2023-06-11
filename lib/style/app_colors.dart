9dd19f883509962dce314817633d29f5a88420dd
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2023-06-11 14:01:05 -0300

    added new colors

--- lib/style/app_colors.dart
+++ lib/style/app_colors.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const white = Colors.white;
   static const black = Colors.black;
+  static const darkGrey = Color(0xFFB7B8B9);
+  static const grey = Color(0xFFD9D9D9);
 }
