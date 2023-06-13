83bfe49d1866499d3a55315465cc8143af04dc7d
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2023-06-13 00:29:44 -0300

    Widget update

--- lib/views/widgets/city_card.dart
+++ lib/views/widgets/city_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:local_turism/data/model/city_model.dart';
-import 'package:local_turism/style/style.dart';
+import 'package:local_turism/views/widgets/photo_carousel_widget.dart';
 import 'package:local_turism/views/widgets/tag_item.dart';
 
 class CityCard extends StatelessWidget {
@@ -14,104 +14,18 @@ class CityCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Padding(
         padding: const EdgeInsets.only(
-          top: 35,
+          top: 25,
           left: 24,
           right: 24,
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(
-              height: 434,
-              width: MediaQuery.of(context).size.width,
-              child: Stack(
-                fit: StackFit.expand,
-                children: [
-                  imageBanner(),
-                  photoText(),
-                ],
-              ),
-            ),
-            dots(),
+            PhotoCarouselWidget(city: city),
             TagItemWidget(tagsList: city.tags),
             const SizedBox(height: 10),
-
-            //! TODO: Inserir uma listview com as fotos no widget rectangle picture
-            //listViewWidget()
-            //rectanglePicture()
+            const Divider(),
           ],
         ),
       );
-
-  Widget dots() => Padding(
-        padding: const EdgeInsets.only(
-          top: 18,
-          bottom: 14,
-        ),
-        child: Container(
-          color: Colors.grey,
-          width: 12,
-          height: 12,
-        ),
-      );
-
-  Widget imageBanner() => Image.network(
-        city.mainImage,
-        fit: BoxFit.cover,
-      );
-
-  Widget photoText() => Padding(
-        padding: const EdgeInsets.only(
-          bottom: 29,
-          left: 21,
-        ),
-        child: Container(
-          alignment: Alignment.bottomLeft,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                city.name,
-                style: Style.bannerCityNameStyle,
-              ),
-              Text(
-                city.cityDescription,
-                style: Style.bannerCityDescriptionStyle,
-                textAlign: TextAlign.start,
-              ),
-            ],
-          ),
-        ),
-      );
-
-  //* widgets que estava usando para testar, não finalizados
-  Widget listViewWidget() => Container(
-        height: 106,
-        width: 320,
-        color: Colors.red,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: 2,
-          itemBuilder: (context, index) => const Text('text'),
-        ),
-      );
-
-  Widget rectanglePicture() {
-    return Padding(
-      padding: const EdgeInsets.only(
-        top: 24,
-        bottom: 38,
-        right: 12,
-      ),
-      child: UnconstrainedBox(
-        child: Container(
-          color: Colors.red,
-          width: 320,
-          height: 106,
-          child: const Text('text'),
-        ),
-      ),
-    );
-  }
 }
