af167e90f855e342ce67777a0ff308f360578cca
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-07-01 16:07:45 -0300

    finished implementation go_router

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:local_turism/data/repository/city_repository.dart';
 import 'package:local_turism/style/style.dart';
 import 'package:local_turism/views/pages/error_page.dart';
 import 'package:local_turism/views/pages/home_page.dart';
 
-void main() {
-  runApp(MainApp());
-}
+void main() => runApp(const MainApp());
+
+final CityRepository _cityRepository = CityRepository();
+
+final GoRouter _router = GoRouter(
+  initialLocation: '/',
+  routes: <RouteBase>[
+    GoRoute(
+      name: 'home',
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return HomePageWidget(
+          repository: _cityRepository,
+        );
+      },
+    ),
+    GoRoute(
+      name: 'errorPage',
+      path: '/errorPage',
+      builder: (BuildContext context, GoRouterState state) {
+        return ErrorPage(
+          repository: _cityRepository,
+        );
+      },
+    ),
+  ],
+);
 
 class MainApp extends StatelessWidget {
-  MainApp({super.key});
-
-  final CityRepository cityRepository = CityRepository();
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Turismo Alagoas',
       debugShowCheckedModeBanner: false,
       theme: Style.appTheme,
-      routes: {
-        '/': (context) => HomePageWidget(repository: cityRepository),
-        '/errorPage': (context) => ErrorPage(repository: cityRepository),
-      },
+      routerConfig: _router,
     );
   }
 }
