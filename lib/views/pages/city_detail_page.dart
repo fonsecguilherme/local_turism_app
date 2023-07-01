c7ff10747f09c5d6da2a843d100504ed39f9c6ba
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-07-01 18:55:55 -0300

    WIP widget creation

--- lib/views/pages/city_detail_page.dart
+++ lib/views/pages/city_detail_page.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:local_turism/data/model/city_model.dart';
+import 'package:local_turism/style/style.dart';
 
 class CityDetailPage extends StatelessWidget {
-  const CityDetailPage({super.key});
+  final City city;
 
-  static const routeName = '/cityDetailPage';
+  const CityDetailPage({super.key, required this.city});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            city.name,
+            style: Style.appBarTextStyle,
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          elevation: 0,
+        ),
+        body: Container(
+          color: Colors.amber,
+        ),
+      ),
     );
   }
 }
