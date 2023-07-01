34bf8645c1a19bd143f8ee45a3f734549eda743f
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-07-01 14:53:42 -0300

    city detail widget created

---
+++ lib/views/pages/city_detail_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CityDetailPage extends StatelessWidget {
+  const CityDetailPage({super.key});
+
+  static const routeName = '/cityDetailPage';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+    );
+  }
+}
