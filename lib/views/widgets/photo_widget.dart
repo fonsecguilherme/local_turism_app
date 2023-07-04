cb8317a7c330b092ab714ef9524c602737a27e77
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-07-04 14:23:48 -0300

    Created custom photo widget component

---
+++ lib/views/widgets/photo_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class PhotoWidget extends StatefulWidget {
+  final String image;
+
+  const PhotoWidget({super.key, required this.image});
+
+  @override
+  State<PhotoWidget> createState() => _PhotoWidgetState();
+}
+
+class _PhotoWidgetState extends State<PhotoWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: Image.network(
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) {
+            return child;
+          }
+          return Center(
+            child: CircularProgressIndicator(
+              value: loadingProgress.expectedTotalBytes != null
+                  ? loadingProgress.cumulativeBytesLoaded /
+                      loadingProgress.expectedTotalBytes!
+                  : null,
+            ),
+          );
+        },
+        widget.image,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
