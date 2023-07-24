218fd1ccc05e5e475fa0b4a461f5b6d99a0f7f16
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2023-07-24 17:51:56 -0300

    new strings added

--- lib/commons/app_strings.dart
+++ lib/commons/app_strings.dart
@@ -1,5 +1,8 @@
 class AppStrings {
   static const appBarText = 'turismo.com';
-  static const errorMessage = 'Ops! Não consegui carregar os dados!';
+  static const errorMessage = 'Ops!\nNão consegui carregar os dados!';
   static const moreInText = 'Mais em ';
+  static const drawerMainScreenText = 'Tela principal';
+  static const drawerAboutMeText = 'Quem sou eu?';
+  static const drawerReferencesText = 'Referências usadas';
 }
