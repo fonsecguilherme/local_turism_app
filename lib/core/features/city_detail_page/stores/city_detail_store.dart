d04be949115d9efd6dc6b6e0b49b6609a51076e8
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2024-04-20 22:43:10 -0300

    Store changes

--- lib/core/features/city_detail_page/stores/city_detail_store.dart
+++ lib/core/features/city_detail_page/stores/city_detail_store.dart
@@ -1,30 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:local_turism/data/models/weather_model.dart';
+import 'package:local_turism/core/features/city_detail_page/temperature_widget_state.dart';
 import 'package:local_turism/data/http_exceptions.dart';
 import 'package:local_turism/domain/weather_repository.dart';
 
-class CityDetailStore {
+class CityDetailStore extends ValueNotifier<TemperatureWidgetState> {
   final IWeatherRepository repository;
 
-  CityDetailStore({required this.repository});
-
-  final ValueNotifier<bool> isLoading = ValueNotifier(false);
-  final ValueNotifier<CityWeatherModel?> state =
-      ValueNotifier<CityWeatherModel?>(null);
-  final ValueNotifier<String> error = ValueNotifier<String>('');
+  CityDetailStore({required this.repository})
+      : super(LoadingTemperatureWidgetState());
 
   Future getCityWeather({required int woeid}) async {
-    isLoading.value = true;
-
     try {
       final result = await repository.getCityWeather(woeid: woeid);
 
-      state.value = result;
+      if (result != null) {
+        value = FetchedTemperatureWidgetState(
+          temperature: result.results,
+        );
+      }
     } on NotFoundException catch (e) {
-      error.value = e.message;
+      value = ErrorTemperatureWidgetState(
+        errorMessage: e.message,
+      );
     } catch (e) {
-      error.value = e.toString();
+      value = ErrorTemperatureWidgetState(
+        errorMessage: e.toString(),
+      );
     }
-    isLoading.value = false;
   }
 }
